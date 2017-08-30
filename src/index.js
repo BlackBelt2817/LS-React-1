dfb212e25619f7bd469fbbda8240e6a1b28c759c
Tyler <blackbelt@yahoo.com>
2017-08-29 20:05:38 -0400

    finished basic to do list

--- src/index.js
+++ src/index.js
@@ -1,7 +1,65 @@
 /*eslint no-unused-vars: "off"*/
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 
-require('!style-loader!css-loader!sass-loader!./index.scss');
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            todo: '',
+            todos: []
+        };
+        this.handleToDoChange = this.handleToDoChange.bind(this);
+        this.addItem = this.addItem.bind(this);
+        this.deleteFirstItem = this.deleteFirstItem.bind(this);
+        this.deleteByNum = this.deleteByNum.bind(this);
+    }
 
-ReactDom.render(<div>Hello World!</div>, document.getElementById('root'));
+    handleToDoChange(event) {
+        let todo = event.target.value;
+        this.setState({ todo });
+    }
+
+    addItem(event) {
+        let todo = this.state.todo;
+        let todos = this.state.todos;
+        todos.push(todo);
+        this.setState({ todos, todo: '' });
+    }
+
+    deleteFirstItem(event) {
+        let todos = this.state.todos;
+        todos.shift();
+        this.setState({ todos });
+    }
+
+    deleteByNum() {
+        let todo = this.state.todo;
+        let todos = this.state.todos;
+        todos.splice(todo - 1, 1);
+        this.setState({todos, todo: ''});
+    }
+
+    render() {
+        return (
+        <div>
+            <h1>Things That Still Need Done:</h1>
+            <input type="text"
+            onChange = {this.handleToDoChange}
+            value={this.state.todo}/>
+             <br />
+             <button onClick={this.addItem}>--------Add Task--------</button><br />
+             <button onClick={this.deleteFirstItem}>I finished The Top Item</button><br />
+             <button onClick={this.deleteByNum}>Delete by #</button>
+             <ol style={{color:"red"}}>
+                 {this.state.todos.map((todo) => {
+                    return <li key={todo}><button>{todo}</button></li>
+                    })}
+             </ol>
+             
+        </div>
+        );
+    }
+}
+
+ReactDom.render(<App />, document.getElementById('root'));
